36d31579cacaac7320db9668dcd47c12b04e48b6
Maciej <maciej.helak@gmail.com>
2020-07-07 21:46:15 +0200

    -

--- src/main/java/helpers/Aes.java
+++ src/main/java/helpers/Aes.java
@@ -7,12 +7,11 @@ public class Aes {
 
     @Test
     public void encryptAny() {
-        System.out.println(Common.encryptAES("user123"));
+        System.out.println(Common.encryptAES("test"));
     }
 
     @Test
     public void decryptAny() {
-        System.out.println(Common.decryptAES("811B7057098AADC30298A5D52FE3E406\n" +
-                "70559A073D99CC7F75ACDA0E6305BA4F"));
+        System.out.println(Common.decryptAES("test"));
     }
 }
